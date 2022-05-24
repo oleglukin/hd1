f776e7d130240c25f5ddc9b0cc2c7022fbd3bd30
Oleg Lukin <oleg.e.lukin@hotmail.com>
2022-05-24 13:00:49 +0500

    fix interface inheritance

--- hd1/Repositories/IOrderRepository.cs
+++ hd1/Repositories/IOrderRepository.cs
@@ -1,8 +1,7 @@
 ﻿using hd1.Models;
-using hd1.Repositories.InMemory;
 
 namespace hd1.Repositories;
 
-public interface IOrderRepository : IInMemoryRepository<int, Order>
+public interface IOrderRepository : IRepository<int, Order>
 {
 }
--- hd1/Repositories/IParcelLockerRepository.cs
+++ hd1/Repositories/IParcelLockerRepository.cs
@@ -1,8 +1,7 @@
 ﻿using hd1.Models;
-using hd1.Repositories.InMemory;
 
 namespace hd1.Repositories;
 
-public interface IParcelLockerRepository : IInMemoryRepository<string, ParcelLocker>
+public interface IParcelLockerRepository : IRepository<string, ParcelLocker>
 {
 }
